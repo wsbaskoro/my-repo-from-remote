2fe094593d11c252805cc8d648d9a8785d07f583
wsbaskoro <wsbaskoro@gmail.com>
2020-08-25 13:05:45 +0700

    "Initial Commit, My First Learning : Basic Web Programming"

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const textfooter = document.getElementsByClassName('footer-font')[0];
+textfooter.innerHTML = '<em> Submission Kelas : Belajar Dasar Pemrograman Web @Dicoding Academy </em>';
+textfooter.style.color = 'white';
